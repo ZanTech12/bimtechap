b3c35e393834389c459d9d1b847d043635a83f96
ZanTech12 <dontechiel@gmail.com>
2026-09-23 14:53:10 +0100

    logo and api changed

--- src/components/admin/PinGenerator.js
+++ src/components/admin/PinGenerator.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../services/api'; // Adjust path if your api file is elsewhere
+import api from '../../services/api'; // Adjust path if your api file is elsewhere
 
 export default function PinGenerator() {
     const [classes, setClasses] = useState([]);
